d2f9650222eec3c5eefa6cf9be1e50f3b7a8e942
aabidihassan <aabidihassan2@gmail.com>
2021-06-18 13:24:28 +0100

    18/06/2021

--- CongeCAM/app/src/main/java/com/example/congecam/MainActivity.java
+++ CongeCAM/app/src/main/java/com/example/congecam/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
